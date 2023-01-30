e38be36ad10064182140b2079ebba055f53b17e3
joshgagnuss <joshgagnuss@gmail.com>
2023-01-30 10:27:47 +0800

    working

--- slideshowCV2.py
+++ slideshowCV2.py
@@ -8,8 +8,10 @@ folder = 'pictures'
 # Get a list of all the photos in the folder
 photos = [photo for photo in os.listdir(folder) if photo.endswith('.jpg')]
 
-# Loop through each photo and display it
-for photo in photos:
-    img = cv2.imread(os.path.join(folder, photo))
-    cv2.imshow('Slideshow', img)
-    cv2.waitKey(20000)  # Display each photo for 3 seconds
+while True:
+    for photo in photos:
+        img = cv2.imread(os.path.join(folder, photo))
+        cv2.namedWindow("Slideshow", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("Slideshow", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow('Slideshow', img)
+        cv2.waitKey(30000)  # Display each photo for 30 seconds
